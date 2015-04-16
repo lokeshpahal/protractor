46f02b9ef17e26a3c2ceaa065aa77bbaf78c5dd5
lokesh.pahal <lokesh.pahal@prodigi.io>
2015-04-16 12:08:16 +0530

    changes

--- scrumdo-login.js
+++ scrumdo-login.js
@@ -1,36 +1,23 @@
 // login scenario
-var cred = {
-	LOGINURL : 'http://integration.scrumdo.com/account/login/'
+var param = {
+	loginUrl : 'https://app.scrumdo.com/account/login/',
+	userName: 'test12',
+	password: 'test',
+	firstTimeLogin: false
 }
 describe('Scrumdo', function() {
 	
 	beforeEach(function() {
-      browser.get(cred.LOGINURL);
+      browser.get(param.loginUrl);
 	  browser.driver.manage().deleteAllCookies();
 	  browser.driver.manage().window().maximize();
     });
 	
-	it('user should able to login', function() {
-		element(by.name('username')).sendKeys('test');
-		element(by.name('password')).sendKeys('test');
-		element(by.css('.scrumdo-btn')).click();
-		
-		element(by.css('.btn-primary')).then(function(){
-			element(by.css('.scrumdo-btn')).click();
-		},function(){});
-		
-		element(by.css('.organization-picker')).then(function(attr){
-			expect(element(by.css('.pull-right')).getText()).toEqual('Create New Organization');
-			console.log('User logged in Successfully!');
-		},function(){
-			expect(element(by.css('.alert-error strong')).getText());
-			console.log('Wrong username or password!');
-		});
-	});
-	
-	it('user should not able to login', function() {
-		element(by.name('username')).sendKeys('test');
-		element(by.name('password')).sendKeys('test');
+	// Case: UnSuccessfull Login 
+	// login with wrong credentials
+	it('UnSuccessfull Login', function() {
+		element(by.name('username')).sendKeys('test12');
+		element(by.name('password')).sendKeys('test12');
 		element(by.css('.scrumdo-btn')).click();
 		
 		element(by.css('.btn-primary')).then(function(){
@@ -45,4 +32,28 @@ describe('Scrumdo', function() {
 		});
 	});
 	
+	// Case: Successfull Login 
+	// login with right credentials
+	it('Successfull Login', function() {
+		element(by.name('username')).sendKeys(param.userName);
+		element(by.name('password')).sendKeys(param.password);
+		element(by.css('.scrumdo-btn')).click();
+		
+		element(by.css('.btn-primary')).then(function(){
+			element(by.css('.scrumdo-btn')).click();
+		},function(){});
+		
+		element(by.css('.nav-settings .dropdown-menu')).then(function(attr){
+			//expect(element(by.css('.nav-settings .dropdown-menu li:nth-child(2) a')).getText()).toEqual('Account Settings');
+			console.log('User logged in Successfully!');
+		},function(){
+			expect(element(by.css('.alert-error strong')).getText());
+			console.log('Wrong username or password!');
+		});
+	});
+	
+	// Case: Create Organization
+	it('Create Organization', function() {
+		
+	});
 });
